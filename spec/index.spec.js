8985fb1e18372f1505b4060ca4e127157cae756b
nattatorn.dev <nattatorn.dev@gmail.com>
2017-10-14 23:15:13 +0700

    :sparkles: support deep equal with object inside array, object inside object

--- spec/index.spec.js
+++ spec/index.spec.js
@@ -14,6 +14,26 @@ describe('strictArrayEquals', () => {
 		expect(strictArrayEquals(['a', 23], [42])).toBe(false);
 	});
 
+	it('should return true when arrays are strictly equal (object inside array)', () => {
+		expect(strictArrayEquals([{1: 'a'}, {2: 'b'}], [{1: 'a'}, {2: 'b'}])).toBe(true);
+		expect(strictArrayEquals([{numbers: [1, 3, 5]}, {numbers: [2, 4, 6]}], [{numbers: [1, 3, 5]}, {numbers: [2, 4, 6]}])).toBe(true);
+	});
+
+	it('should return false when arrays are not strictly equal (object inside array)', () => {
+		expect(strictArrayEquals([{1: 'a'}], [{1: 'b'}])).toBe(false);
+		expect(strictArrayEquals([{numbers: [1, 3, 5]}, {numbers: [2, 4, 6]}], [{numbers: [1, 3, 5]}])).toBe(false);
+	});
+
+	it('should return true when arrays are strictly equal (object inside object)', () => {
+		expect(strictArrayEquals([{1: {id: '1'}}], [{1: {id: '1'}}])).toBe(true);
+		expect(strictArrayEquals([{numbers: {1: 1, 2: 2}}], [{numbers: {1: 1, 2: 2}}])).toBe(true);
+	});
+
+	it('should return false when arrays are not strictly equal (object inside object)', () => {
+		expect(strictArrayEquals([{1: {id: '1'}}], [{1: {id: '2'}}])).toBe(false);
+		expect(strictArrayEquals([{numbers: {1: 1, 2: 2}}], [{numbers: {1: 1}}])).toBe(false);
+	});
+
 	it('should return false when only one array is provided as an argument', () => {
 		expect(strictArrayEquals(['a', 23])).toBe(false);
 	});
